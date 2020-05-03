247d0880ebc01320677f203dcbc3d4188d926a1b
zcmee <zcm@interia.pl>
2020-05-03 13:38:18 +0200

    add test controller with html page

--- src/main/java/zcm/com/github/SpringSecurity/controllers/WebController.java
+++ src/main/java/zcm/com/github/SpringSecurity/controllers/WebController.java
@@ -3,14 +3,16 @@ package zcm.com.github.SpringSecurity.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("user/panel")
 public class WebController {
 
     @GetMapping
-    public String exmpleMessage() {
-        return "Twoja stara zapierdala";
+    public ModelAndView exmpleMessage(ModelAndView modelAndView) {
+        modelAndView.setViewName("user_panel");
+        return modelAndView;
     }
 
 }
