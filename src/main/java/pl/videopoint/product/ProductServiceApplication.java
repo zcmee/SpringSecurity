26561a97e0a62eeef40af8d1ab6a00837c911f83
zcmee <zcm@interia.pl>
2020-05-24 17:21:03 +0200

    domyślna konfiguracja z własna generacja hashowanego hasła

--- src/main/java/pl/videopoint/product/ProductServiceApplication.java
+++ src/main/java/pl/videopoint/product/ProductServiceApplication.java
@@ -2,6 +2,9 @@ package pl.videopoint.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ProductServiceApplication {
@@ -9,5 +12,11 @@ public class ProductServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
     }
+
+    //Mozemy nadpisac domyślny sposób generacji hasku hasła
+//    @Bean
+//    public PasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder(10);
+//    }
 
 }
